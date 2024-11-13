0c47724368cbf92a0ad361bfc0de5ad5f7ab412e
gabmota8 <gabmota8@hotmail.com>
2024-11-12 22:00:04 -0300

    alteração no no service

---
+++ sistema-leilao/src/test/java/com/leilao/service/ClienteServiceTest.java
@@ -0,0 +1,25 @@
+package com.leilao.service;
+
+import org.junit.jupiter.api.Test;
+
+public class ClienteServiceTest {
+    @Test
+    void testBuscarPorId() {
+
+    }
+
+    @Test
+    void testCadastrarCliente() {
+
+    }
+
+    @Test
+    void testExcluirCliente() {
+
+    }
+
+    @Test
+    void testListarTodos() {
+
+    }
+}
